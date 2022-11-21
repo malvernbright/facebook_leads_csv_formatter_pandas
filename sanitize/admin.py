01c493c6c658e3d49cb0fe166c481850dc0c5162
malvernbright <malvernbright.95@gmail.com>
2022-11-21 16:42:26 +0200

    Added a model

--- sanitize/admin.py
+++ sanitize/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import FbFile
 
-# Register your models here.
+
+@admin.register(FbFile)
+class FbFileAdmin(admin.ModelAdmin):
+    list_display = ['file_name', 'date_added']
